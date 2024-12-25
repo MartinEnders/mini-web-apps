0282f364aa779eb2247705db129d86e78f15bc53
Dr. Martin R. Enders <martin@di2.io>
2024-12-25 10:39:54 +0100

    minor

--- src/app.py
+++ src/app.py
@@ -39,8 +39,7 @@ Tel: 0049 157 321 458 72
 
 sm_zusammen = date(2011, 6, 12)
 sm_verheirated = date(2019, 4, 25)
-sm_jenaer_str = date(2012, 3, 17)
-sm_linnea = date(2020, 6, 4)
+sm_haus = date(2024, 9, 27)
 
 gc_verheirated = date(2015, 12, 23)
 
@@ -59,17 +58,13 @@ def gc_verheiratet_seit():
     tage = get_heute() - gc_verheirated
     return "Conny und Georg sind seit {} Tagen verheiratet.".format(tage.days)
 
-def sm_linnea_seit():
-    tage = get_heute() - sm_linnea
-    return "Linnea ist {} Tage ({} Wochen) alt.".format(tage.days, int(tage.days/7))
-
-def sm_jenaer_str_seit():
-    tage = get_heute() - sm_jenaer_str
-    return "Sophia und Martin wohnen seit {} Tagen zusammen in der Jenaer Str. in Erlangen.".format(tage.days)
+def sm_haus_seit():
+    tage = get_heute() - sm_haus
+    return "Sophia und Martin wohnen seit {} Tagen zusammen in einem Häuschen in Kronach.".format(tage.days)
 
 @app.route('/sm')
 def sm():
-    facts = [sm_zusammen_seit(), sm_jenaer_str_seit(), sm_verheiratet_seit(), sm_linnea_seit()]
+    facts = [sm_zusammen_seit(), sm_haus_seit(), sm_verheiratet_seit()]
     html_facts = "\n".join(["<div>"+f+"</div>" for f in facts])
     return html_template.format("Sophia und Martin Facts", html_facts)
 
